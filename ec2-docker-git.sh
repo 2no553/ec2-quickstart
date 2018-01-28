eab2d9401a5477a64dad9572a2d0e0e6d09ebd6f
二宮良太 <leyline.0813@live.jp>
2018-01-29 00:50:54 +0900

    init

---
+++ ec2-docker-git.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# パッケージ更新
+sudo yum update -y
+
+# 自動更新設定
+sudo yum install yum-cron -y
+sudo chkconfig yum-cron on
+sudo sed -i "s/^apply_updates.*$/apply_updates = yes/g" /etc/yum/yum-cron.conf
+sudo service yum-cron start
+
+
+# ローカルタイム・ハードウェアクロック設定
+sudo ln -sf /usr/share/zoneinfo/Japan /etc/localtime
+sudo sed -i "s/\"UTC\"/\"Japan\"/g" /etc/sysconfig/clock
+
+# 文字コード設定
+sudo sed -i "s/en_US\.UTF-8/ja_JP\.UTF-8/g" /etc/sysconfig/i18n
+
+# docker設定
+sudo yum install -y docker
+sudo chkconfig docker on
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# git設定
+sudo yum install -y git
+
+# システム再起動
+sudo reboot
