0a1b62718b536ebbb4035f0e4a95fbd5922b3a36
2no553 <ninomiya.ryota@gmail.com>
2018-07-16 18:54:02 +0900

    add docker git

--- ec2-quickstart.sh
+++ ec2-quickstart.sh
@@ -18,3 +18,15 @@ sudo timedatectl set-timezone Asia/Tokyo
 localectl status
 sudo localectl set-locale LANG=ja_JP.UTF-8
 sudo localectl set-keymap jp106
+
+#gitインストール
+sudo yum install git -y
+
+#dockerインストール
+sudo yum install docker -y
+sudo systemctl status docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
